c565f509ec841cc6ae76697210404a81c765daf9
stefangohlke <gohlke.stefan@yahoo.de>
2020-10-06 15:46:17 +0200

    Initial Commit

---
+++ src/personservice.ts
@@ -0,0 +1,29 @@
+export class Person {
+  id: number
+  lastName: string = "UNKNOWN"
+  firstName: string = "UNKNOWN"
+  age: number = 999
+
+  constructor(id: number, lastName: string, firstName: string, age: number) {
+    this.id = id
+    this.lastName = lastName
+    this.firstName = firstName
+    this.age = age
+  }
+}
+
+const personList = new Map([
+    ["1", new Person(1, "Meier", "Hans", 40)],
+    ["2", new Person(2, "Mahler", "Sandra", 32)],
+    ["3", new Person(3, "Huber", "Franz", 56)],
+])
+
+export class PersonService{
+  static getAllPersons(): Person[] {
+    return Array.from(personList.values())
+  }
+
+  static getPersonForId(id: string): Person {
+    return personList.get(id)
+  }
+}
